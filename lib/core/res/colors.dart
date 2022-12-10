31b7f70a48b59e49d4492f3c2ae9860d89af5912
alif akbar <alif.akbar@grabtaxi.com>
2022-12-10 16:32:37 +0700

    init

---
+++ lib/core/res/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// contains all colors on app
+mixin PokeColor {
+  /// Text
+  static const black = Color(0xFF42494D);
+  static const grey = Color(0xFF7B8082);
+
+  /// BGs
+  static const yellow = Color(0xFFE6AB09);
+
+  /// Pokemon Type
+  static const fire = Color(0xFFDE2C2C);
+  static const plant = Color(0xFF01B956);
+  static const water = Color(0xFF0571A6);
+}
---
+++ lib/core/res/strs.dart
@@ -0,0 +1,22 @@
+/// contains all string on app
+mixin PokeText {
+  /// Welcome Screen
+  static const heroText = "All the Pokémon data you'll ever need in one place!";
+  static const welcomeDesc = "Thousands of data compiled into one place";
+  static const welcomeButton = "Check PokèDex";
+
+  /// Pokedex List
+  static const pokedexTitle = "PokèDex";
+  static const pokedexDesc = "All Generation totaling %d Pokemon";
+  static const loadMore = "Load More ...";
+
+  /// Pokemon Detail
+  static const weight = "Weight";
+  static const height = "Height";
+  static const abilities = "Abilities";
+  static const pokeType = "Type";
+  static const moreDetail = "More Detail";
+  static const otherImages = "Other Images";
+  static const stat = "Stats";
+  static const evolution = "Evolution";
+}
